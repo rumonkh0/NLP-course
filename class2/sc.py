91e11862b7d499917e15a9b7284c96dd13a7d5f5
rumonkh0 <rummankh0@gmail.com>
2025-04-03 00:36:22 +0600

    class 2 assignment done

---
+++ class2/sc.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Set up the Chrome driver
+driver = webdriver.Chrome()
+
+# Open the URL
+driver.get('https://www.rokomari.com/search?affId=oMKaMO69Or0K6kM&affs=33842&cma=604800&gad_source=1&gclid=CjwKCAjwwLO_BhB2EiwAx2e-31PNzkVqRRnogZbyeCM9V9u6B1K6c3wjg75LpviHxrh4LeagLbQVIRoC-NwQAvD_BwE&term=science&search_type=ALL&page=1')
+
+# scroll to bottom for image loading
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")                    
+
+textlist = []
+imagelinks = [] 
+driver.maximize_window()  
+time.sleep(7)                 
+for i in range(1 , 61):
+    ele = str(i)
+    text = driver.find_element(By.XPATH, f"/html/body/div[7]/div/div/div/section[2]/div[2]/div/div[{ele}]/div/a/div[2]/h4").text
+    image = driver.find_element(By.XPATH, f"/html/body/div[7]/div/div/div/section[2]/div[2]/div/div[{ele}]/div/a/div[1]/img").get_attribute('src')
+    textlist.append(text)
+    imagelinks.append(image)
+
+
+print('--------------------------------titles--------------------------------')
+for text in textlist:
+    print(text)
+print()
+print('--------------------------------imagelinks--------------------------------')
+for image in imagelinks:
+    print(image)
+      
+driver.quit()
---
+++ keywords.md
@@ -0,0 +1,11 @@
+### **webscrapping**
+
+selenium
+puputter
+cypress
+playwrite
+beautiful soup
+
+### Locator type   // This is the valuable topics for mastering webscripping
+
+xpath
